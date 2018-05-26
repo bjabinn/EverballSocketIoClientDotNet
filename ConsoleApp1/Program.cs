bbf42ec8f6790000b243b50470f6ed1d9597a887
Jose Ant. Beltrán Márquez <bjabinn@hotmail.com>
2018-05-26 19:40:23 +0200

    Sufriendo...

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,90 +1,16 @@
 ﻿using System;
-using Quobject.SocketIoClientDotNet.Client;
-using Newtonsoft.Json;
-using System.Threading;
 
 namespace EverballDotNet
 {
     class Program
     {
-        const int timeToWait = 150;
-        static object dataFromServer;
-        static Socket socket;
-
-        static private void Connect()
-        {
-            socket = IO.Socket("http://localhost:3000");
-            socket.Connect();
-            Console.WriteLine("Connected to server");
-        }
-
-        static private void Play()
-        {
-            socket.On(Socket.EVENT_CONNECT_ERROR, () =>
-            {
-                Console.WriteLine("Error connecting");
-            });
-
-            socket.On(Socket.EVENT_CONNECT_TIMEOUT, () =>
-            {
-                Console.WriteLine("Timeout error");
-            });
-
-            socket.On(Socket.EVENT_RECONNECTING, () =>
-            {
-                Console.WriteLine("Reconnecting");
-            });
-
-            socket.On("connect", () =>
-            {
-                Console.WriteLine("connect: Recibido evento connect desde el server");
-                var login = new Login
-                {
-                    userName = "1234568",
-                    userPassword = "1234568"
-                };
-
-                var datoToSend = JsonConvert.SerializeObject(login);
-                socket.Emit("login", datoToSend);
-
-            });
-
-            socket.On("server_message", (data) =>
-            {
-                Console.WriteLine($"server_message: {data}");
-                var roomData = new Room
-                {
-                    RoomName = "One__",
-                    RoomPassword = "123"
-                };
-                var datoToSend = JsonConvert.SerializeObject(roomData);
-                socket.Emit("join_room", datoToSend);
-            });
-
-            socket.On("match_start", (data) =>
-            {
-                Console.WriteLine($"match_start - {data}");
-                dataFromServer = data;
-            });
-
-            socket.On("server_state", (data) =>
-            {
-                Console.WriteLine($"server_state: {data} ");
-                var mov = new Movement
-                {
-                    Angle = 90.0,
-                    Force = 0.1,
-                    Toy = 1
-                };
-                socket.Emit("client_input", JsonConvert.SerializeObject(mov));
-                //Thread.Sleep(timeToWait);
-            });
-        }
+        static SocketIoManager socket = new SocketIoManager("http://localhost:3000");
 
         static void Main(string[] args)
         {
-            Connect();
-            Play();
+            socket.Conecta();
+            socket.Play();
+            Console.ReadLine();
         } //end Main
 
     } //end class
---
+++ ConsoleApp1/SocketIoManager.cs
@@ -0,0 +1,91 @@
+﻿using Quobject.SocketIoClientDotNet.Client;
+using System;
+using Newtonsoft.Json;
+
+namespace EverballDotNet
+{
+    class SocketIoManager
+    {
+        private Socket _socket;
+        object _matchData;
+
+        public SocketIoManager(string url)
+        {            
+            _socket = IO.Socket(url);
+        }
+
+        public void Conecta()
+        {
+            _socket.Connect();
+        }
+
+        public void Play()
+        {
+
+            _socket.On("connect", () =>
+            {
+                Console.WriteLine("connect: Recibido evento connect desde el server");
+                var login = new { name = "123456", password = "123456" };
+                _socket.Emit("login", login);
+            });
+
+            _socket.On("server_message", (data) =>
+            {
+                var dataStr = data as string;
+                Console.WriteLine($"server_message: {data}");
+
+                if (dataStr.IndexOf("Logged in as") == 0)
+                {
+                    var join = new { name = "One__", password = "123" };
+                    _socket.Emit("join_room", join);
+                }
+            });
+
+            _socket.On("match_start", (msg) =>
+            {
+                Console.WriteLine($"match_start: {msg}");
+
+                //var json = JsonConvert.SerializeObject(msg);
+                //_matchStart = JsonConvert.DeserializeObject<_matchData>(json);
+            });
+
+            _socket.On("server_state", (msg) =>
+            {
+                Console.WriteLine($"serverState: {msg}");
+
+                //var json = JsonConvert.SerializeObject(msg);
+                //var serverState = JsonConvert.DeserializeObject<ServerState>(json);
+
+                //if (_matchStart?.Role == 2)
+                //{
+                //    PlayAsPlayer2(serverState);
+                //}
+                //else
+                //{
+                //    PlayAsPlayer1(serverState);
+                //}
+            });
+
+            _socket.On("connect_error", (exception) =>
+            {
+                var ex = exception as Exception;
+                Console.WriteLine($"Error: {ex?.InnerException?.Message}");
+            });
+        }
+
+        //private PlayAsPlayer1()
+        //{
+
+        //}
+
+        //private PlayAsPlayer2()
+        //{
+
+        //}
+
+        public void Disconnect()
+        {
+            _socket?.Disconnect();
+        }
+    } // end class
+} //end namespace
