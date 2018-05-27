50b0d2eb7a0273ce82270cc2df8ee69de48e34af
Jose Ant. Beltrán Márquez <bjabinn@hotmail.com>
2018-05-27 23:19:49 +0200

    probando

--- ConsoleApp1/SocketIoManager.cs
+++ ConsoleApp1/SocketIoManager.cs
@@ -19,7 +19,7 @@ namespace EverballDotNet
         private Lado jugandoComo;
 
         public SocketIoManager(string url)
-        {            
+        {
             _socket = IO.Socket(url);
         }
 
@@ -46,6 +46,7 @@ namespace EverballDotNet
                 if (dataStr.IndexOf("Logged in as") == 0)
                 {
                     var join = new { name = "One__", password = "123" };
+                    //var join = new { name = "Test room", password = "abc" };
                     _socket.Emit("join_room", join);
                 }
             });
@@ -71,7 +72,7 @@ namespace EverballDotNet
                 var json = JsonConvert.SerializeObject(msg);
                 WriteLog($"match_start: {msg}");
                 _matchData = JsonConvert.DeserializeObject<MatchData>(json);
-                jugandoComo = (Lado)(_matchData?.role);               
+                jugandoComo = (Lado)(_matchData?.role);
             });
 
             _socket.On("connect_error", (exception) =>
@@ -81,28 +82,98 @@ namespace EverballDotNet
             });
         }
 
+        //jugando a la derecha
         private void PlayAsPlayer1(ServerState serverState)
         {
-            WriteLog("PlayAsPlayer1" + JsonConvert.SerializeObject(serverState));
+            //DosPorLosOponentes_1PorElBalon(serverState);
         }
 
+        //a la izqda
         private void PlayAsPlayer2(ServerState serverState)
         {
-            //Chapa 1 y 2 van por sus chapas
-            var mov1 = new CapMovement()
+            DosPorLosOponentes_1PorElBalon(serverState);
+        }
+
+        private void DosPorLosOponentes_1PorElBalon(ServerState serverState)
+        {
+            if (serverState.Ball.x < 4.3)
+            {                
+                if (serverState.Team_2[0].cooldown <= 0)
+                {
+                    var mov1 = new CapMovement()
+                    {
+                        angle = (float)(Math.Atan2(1.5 - serverState.Team_2[0].y, 1.15 - serverState.Team_2[0].x) * 180 / Math.PI),
+                        cap_num = 1,
+                        force = 1.2f
+                    };
+                    _socket.Emit("client_input", mov1);
+                }
+
+                if (serverState.Team_2[1].cooldown <= 0)
+                {
+                    var mov2 = new CapMovement()
+                    {
+                        angle = (float)(Math.Atan2(2.5 - serverState.Team_2[1].y, 1.15 - serverState.Team_2[1].x) * 180 / Math.PI),
+                        cap_num = 2,
+                        force = 1.2f
+                    };
+
+                    _socket.Emit("client_input", mov2);
+                }
+
+                //chapa 3 va por la bola
+                if (serverState.Team_2[2].cooldown <= 0)
+                {
+                    var mov3 = new CapMovement()
+                    {
+                        angle = (float)(Math.Atan2(3.5 - serverState.Team_2[2].y, 1.15 - serverState.Team_2[2].x) * 180 / Math.PI),
+                        cap_num = 3,
+                        force = 1.2f
+                    };
+
+                    _socket.Emit("client_input", mov3);
+                }
+            }
+            else
             {
-                angle = 45,
-                cap_num = 1,
-                force = 1.2f
-            };
-            if (serverState.Team_2[0].cooldown == 0)
-            {
-                _socket.Emit("join_room", mov1);
+                //Chapa 1 y 2 van por sus chapas
+                if (serverState.Team_2[0].cooldown <= 0)
+                {
+                    var mov1 = new CapMovement()
+                    {
+                        angle = (float)(Math.Atan2(serverState.Team_1[0].y - serverState.Team_2[0].y, serverState.Team_1[0].x - serverState.Team_2[0].x) * 180 / Math.PI),
+                        cap_num = 1,
+                        force = 1.2f
+                    };
+                    _socket.Emit("client_input", mov1);
+                }
+
+                if (serverState.Team_2[1].cooldown <= 0)
+                {
+                    var mov2 = new CapMovement()
+                    {
+                        angle = (float)(Math.Atan2(serverState.Team_1[1].y - serverState.Team_2[1].y, serverState.Team_1[1].x - serverState.Team_2[1].x) * 180 / Math.PI),
+                        cap_num = 2,
+                        force = 1.2f
+                    };
+
+                    _socket.Emit("client_input", mov2);
+                }
+
+                //chapa 3 va por la bola
+                if (serverState.Team_2[2].cooldown <= 0)
+                {
+                    var mov3 = new CapMovement()
+                    {
+                        angle = (float)(Math.Atan2(serverState.Ball.y - serverState.Team_2[2].y, serverState.Ball.x - serverState.Team_2[2].x) * 180 / Math.PI),
+                        cap_num = 3,
+                        force = 1.2f
+                    };
+
+                    _socket.Emit("client_input", mov3);
+                }
             }
             
-
-            //chapa 3 va por la bola
-
         }
 
         public void Disconnect()
